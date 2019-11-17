4aacfb1992417d4571af2913fc544af57050dca2
Zidan <zidan@Zidans-MacBook-Pro.local>
2019-11-17 01:17:11 -0500

    initialized ORM and created first table in database

--- application.py
+++ application.py
@@ -4,30 +4,38 @@ from flask import Flask, session, render_template, request, flash
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base  
 import requests
 import json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 KEY = 'Cc5azpsc8j0fATmIxBfhDw'
 
 
-# Check for environment variable
-os.environ['DATABASE_URL'] = 'postgres://wscxawtmtqknwv:500660f9d795a48114d654dd906ed720f12db842c371b2239ad21bedd5f8d04f@ec2-174-129-253-45.compute-1.amazonaws.com:5432/d6et227qpa435r'
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wscxawtmtqknwv:500660f9d795a48114d654dd906ed720f12db842c371b2239ad21bedd5f8d04f@ec2-174-129-253-45.compute-1.amazonaws.com:5432/d6et227qpa435r'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
+# database schema
+class User(db.Model):
 
-    # res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": KEY, "isbns": "1632168146"})
-    # x = res.json()
-    # return x
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(65), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}'.format(self.username)
+
+
+
 
 @app.route("/")
 def index():
