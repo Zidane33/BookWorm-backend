af691720bffa9392dd7eef040ab4f8a5f17cd41c
Zidan <midoz393@gmail.com>
2020-01-18 13:28:33 -0500

    debugging env variables

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-from boto.s3.connection import S3Connection
 import os
 
 
@@ -10,9 +9,9 @@ app = Flask(__name__)
 login = LoginManager(app)
 
 # database config
-app.config['SECRET_KEY'] = S3Connection(os.environ['SECRET'])
-app.config['SQLALCHEMY_DATABASE_URI'] = S3Connection(os.environ['DATABASE_URL'])
-app.config['PASSWORD'] = S3Connection(os.environ['PASSWORD'])
+app.config['SECRET_KEY'] = (os.environ['SECRET'])
+app.config['SQLALCHEMY_DATABASE_URI'] = (os.environ['DATABASE_URL'])
+app.config['PASSWORD'] = (os.environ['PASSWORD'])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
